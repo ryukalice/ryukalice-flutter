50983801e29511f46637ac2c110749c585c9ce12
kokoriru <yuka@ryukalice.com>
2022-08-13 19:23:21 +0900

    URL の # を取り除く

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 import '/utils/routes.dart';
 
 void main() {
+  setUrlStrategy(PathUrlStrategy());
   runApp(const MyApp());
 }
 
