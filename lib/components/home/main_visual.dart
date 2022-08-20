fa642367aac1f272b26bdfcaac04521ccdc0a10f
kokoriru <yuka@ryukalice.com>
2022-08-21 00:40:45 +0900

    ホームの各要素の枠を組む

---
+++ lib/components/home/main_visual.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class MainVisual extends StatelessWidget {
+  const MainVisual({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Text('MainVisual');
+  }
+}
