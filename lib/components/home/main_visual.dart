2a35b58545b99f3242a05cee7b8184c85df5dce7
kokoriru <yuka@ryukalice.com>
2022-08-22 13:25:30 +0900

    文章のまわりの余白を設定

--- lib/components/home/main_visual.dart
+++ lib/components/home/main_visual.dart
@@ -19,18 +19,21 @@ class MainVisual extends StatelessWidget {
               ),
             ),
           ),
-          child: Column(
-            children: const [
-              Text(
-                'Ruby, Ruby on Rails, Python, Django\n'
-                'Flutter, PHP, WordPress, CakePHP, Laravel\n'
-                'JavaScript, React.js, Next.js, Vue.js, React Native\n'
-                'Microsoft Azure, Heroku, Vercel, AWS\n'
-                'C, TRON, C++, C++Builder, Delphi, Java',
-              ),
-              SizedBox(height: 100),
-              Text('Ryukalice'),
-            ],
+          child: Padding(
+            padding: const EdgeInsets.only(top: 120, right: 30, bottom: 50),
+            child: Column(
+              children: const [
+                Text(
+                  'Ruby, Ruby on Rails, Python, Django\n'
+                  'Flutter, PHP, WordPress, CakePHP, Laravel\n'
+                  'JavaScript, React.js, Next.js, Vue.js, React Native\n'
+                  'Microsoft Azure, Heroku, Vercel, AWS\n'
+                  'C, TRON, C++, C++Builder, Delphi, Java',
+                ),
+                SizedBox(height: 100),
+                Text('Ryukalice'),
+              ],
+            ),
           ),
         ),
       ],
