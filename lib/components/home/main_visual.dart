b6ed8434b7b4be257c013c12244ebf11591aa579
kokoriru <yuka@ryukalice.com>
2022-08-22 13:10:51 +0900

    ホームのメインビジュアル画像追加

--- lib/components/home/main_visual.dart
+++ lib/components/home/main_visual.dart
@@ -5,6 +5,6 @@ class MainVisual extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Text('MainVisual');
+    return Image.asset('assets/home/alice.png');
   }
 }
