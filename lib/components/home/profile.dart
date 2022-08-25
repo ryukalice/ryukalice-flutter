7ed9b474794f1104bd486ffd158c69bdf515756b
kokoriru <yuka@ryukalice.com>
2022-08-25 18:26:02 +0900

    文章内のリンク[ブログ]のみ underline を入れる

--- lib/components/home/profile.dart
+++ lib/components/home/profile.dart
@@ -31,11 +31,23 @@ class _State extends State<Profile> {
           Text('Profile', style: _h1Style, textAlign: TextAlign.center),
           Text('プロフィール', style: _h2Style, textAlign: TextAlign.center),
           const SizedBox(height: 30),
-          Text(
-            '夫婦でシステム開発のお仕事をしています。福岡県行橋市で猫3匹と生活しています。'
-            '現在は自社サービス開発企業に夫婦で勤め、Ruby on Rails と Next.js のシステム開発をしています。'
-            '副業で個人事業主としてシステム開発の仕事や、開発者教育の仕事をしています。ブログを書いています。',
-            style: _bodyStyle
+          Text.rich(
+            TextSpan(
+              text:
+                '夫婦でシステム開発のお仕事をしています。福岡県行橋市で猫3匹と生活しています。'
+                '現在は自社サービス開発企業に夫婦で勤め、Ruby on Rails と Next.js のシステム開発をしています。'
+                '副業で個人事業主としてシステム開発の仕事や、開発者教育の仕事をしています。',
+              style: _bodyStyle,
+              children: const <TextSpan>[
+                TextSpan(
+                  text: 'ブログ',
+                  style: TextStyle(
+                    decoration: TextDecoration.underline,
+                  )
+                ),
+                TextSpan(text: 'を書いています。'),
+              ],
+            ),
           ),
         ],
       ),
