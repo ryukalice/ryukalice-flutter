bc3ef41822baa0d0391b7f54232fa6eb03385391
kokoriru <yuka@ryukalice.com>
2022-08-25 18:16:44 +0900

    スタイリング

--- lib/components/home/profile.dart
+++ lib/components/home/profile.dart
@@ -8,19 +8,34 @@ class Profile extends StatefulWidget {
 }
 
 class _State extends State<Profile> {
+  final _h1Style = const TextStyle(
+    fontSize: 24,
+    letterSpacing: 2,
+    fontWeight: FontWeight.bold,
+  );
+
+  final _h2Style = const TextStyle(
+    fontSize: 9,
+    letterSpacing: 2,
+    fontWeight: FontWeight.bold,
+  );
+
+  final _bodyStyle = const TextStyle(letterSpacing: 1.5);
+
   @override
   Widget build(BuildContext context) {
     return Container(
       padding: const EdgeInsets.only(top: 50, bottom: 50),
       child: Column(
-        children: const [
-          Text('Profile'),
-          Text('プロフィール'),
-          SizedBox(height: 30),
+        children: [
+          Text('Profile', style: _h1Style, textAlign: TextAlign.center),
+          Text('プロフィール', style: _h2Style, textAlign: TextAlign.center),
+          const SizedBox(height: 30),
           Text(
             '夫婦でシステム開発のお仕事をしています。福岡県行橋市で猫3匹と生活しています。'
             '現在は自社サービス開発企業に夫婦で勤め、Ruby on Rails と Next.js のシステム開発をしています。'
             '副業で個人事業主としてシステム開発の仕事や、開発者教育の仕事をしています。ブログを書いています。',
+            style: _bodyStyle
           ),
         ],
       ),
