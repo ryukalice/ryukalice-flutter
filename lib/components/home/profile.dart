1f97c26766e27341153cdf5ffa912dd666f9842c
kokoriru <yuka@ryukalice.com>
2022-08-25 18:13:58 +0900

    Container に内包し上下の padding を設定

--- lib/components/home/profile.dart
+++ lib/components/home/profile.dart
@@ -10,17 +10,20 @@ class Profile extends StatefulWidget {
 class _State extends State<Profile> {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: const [
-        Text('Profile'),
-        Text('プロフィール'),
-        SizedBox(height: 30),
-        Text(
-          '夫婦でシステム開発のお仕事をしています。福岡県行橋市で猫3匹と生活しています。'
-          '現在は自社サービス開発企業に夫婦で勤め、Ruby on Rails と Next.js のシステム開発をしています。'
-          '副業で個人事業主としてシステム開発の仕事や、開発者教育の仕事をしています。ブログを書いています。',
-        ),
-      ],
+    return Container(
+      padding: const EdgeInsets.only(top: 50, bottom: 50),
+      child: Column(
+        children: const [
+          Text('Profile'),
+          Text('プロフィール'),
+          SizedBox(height: 30),
+          Text(
+            '夫婦でシステム開発のお仕事をしています。福岡県行橋市で猫3匹と生活しています。'
+            '現在は自社サービス開発企業に夫婦で勤め、Ruby on Rails と Next.js のシステム開発をしています。'
+            '副業で個人事業主としてシステム開発の仕事や、開発者教育の仕事をしています。ブログを書いています。',
+          ),
+        ],
+      ),
     );
   }
 }
