499bd32b07d490745d2bdac18cb032150147d409
kokoriru <yuka@ryukalice.com>
2022-08-25 18:10:22 +0900

    プロフィール文追加

--- lib/components/home/profile.dart
+++ lib/components/home/profile.dart
@@ -10,6 +10,16 @@ class Profile extends StatefulWidget {
 class _State extends State<Profile> {
   @override
   Widget build(BuildContext context) {
-    return const Text('Profile');
+    return Column(
+      children: const [
+        Text('Profile'),
+        Text('プロフィール'),
+        Text(
+          '夫婦でシステム開発のお仕事をしています。福岡県行橋市で猫3匹と生活しています。'
+          '現在は自社サービス開発企業に夫婦で勤め、Ruby on Rails と Next.js のシステム開発をしています。'
+          '副業で個人事業主としてシステム開発の仕事や、開発者教育の仕事をしています。ブログを書いています。',
+        ),
+      ],
+    );
   }
 }
