4c38afda3317724e057179ee67d9a58b2f443ad7
kokoriru <yuka@ryukalice.com>
2022-08-25 18:10:48 +0900

    タイトルと本文の間に余白を追加

--- lib/components/home/profile.dart
+++ lib/components/home/profile.dart
@@ -14,6 +14,7 @@ class _State extends State<Profile> {
       children: const [
         Text('Profile'),
         Text('プロフィール'),
+        SizedBox(height: 30),
         Text(
           '夫婦でシステム開発のお仕事をしています。福岡県行橋市で猫3匹と生活しています。'
           '現在は自社サービス開発企業に夫婦で勤め、Ruby on Rails と Next.js のシステム開発をしています。'
