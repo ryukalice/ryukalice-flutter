706ab268997f4bbeb1fd0878efe6dba0890fc8f7
kokoriru <yuka@ryukalice.com>
2022-09-22 23:42:55 +0900

    Skills のタイトルをスタイリング

--- lib/components/home/skills.dart
+++ lib/components/home/skills.dart
@@ -8,8 +8,28 @@ class Skills extends StatefulWidget {
 }
 
 class _State extends State<Skills> {
+  final _h1Style = const TextStyle(
+    fontSize: 24,
+    letterSpacing: 2,
+    fontWeight: FontWeight.bold,
+  );
+
+  final _h2Style = const TextStyle(
+    fontSize: 9,
+    letterSpacing: 2,
+    fontWeight: FontWeight.bold,
+  );
+
   @override
   Widget build(BuildContext context) {
-    return const Text('Skills');
+    return Container(
+      padding: const EdgeInsets.only(top: 50, bottom: 150),
+      child: Column(
+        children: [
+          Text('Skills', style: _h1Style, textAlign: TextAlign.center),
+          Text('得意分野', style: _h2Style, textAlign: TextAlign.center),
+        ],
+      ),
+    );
   }
 }
